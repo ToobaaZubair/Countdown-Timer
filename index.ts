012b678566d3fa467fc96cbf53f923432a6d51f7
ToobaaZubair <toobaazubair@gmail.com>
2024-02-12 22:04:43 +0500

    timer

---
+++ index.ts
@@ -0,0 +1,15 @@
+function countdowntimer(second:number):void{
+    let remainingtime=second
+    const timeInterval=setInterval(()=>{
+        if(remainingtime<=0){
+            clearInterval(timeInterval)
+            console.log(`Countdown finished!!!!!!`);
+            
+        }
+        else{
+            console.log(`${remainingtime} seconds remaining.......`);
+            remainingtime--
+        }
+    },1000)
+}
+countdowntimer(60)
